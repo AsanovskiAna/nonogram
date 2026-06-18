eec08972ff6589086badbf6b82cd2bbe8dc8d700
Ana Asanovski <ana.1.asanovski@gmail.com>
2026-06-18 13:02:43 +0200

    Small fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
+  preload: false,
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
